9e2be1d0bd4de106e8c55d3791e6b436593d5925
example <email@example.com>
2026-07-28 17:42:37 +0300

    keep tariff grpc service running

--- subscriber-tariff-service/src/main/java/com/example/tariff/SubscriberTariffApplication.java
+++ subscriber-tariff-service/src/main/java/com/example/tariff/SubscriberTariffApplication.java
@@ -3,6 +3,8 @@ package com.example.tariff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 @SpringBootApplication
 public class SubscriberTariffApplication {
 
@@ -10,6 +12,7 @@ public class SubscriberTariffApplication {
         var context = SpringApplication.run(SubscriberTariffApplication.class, args);
         try {
             context.getBean(SubscriberTariffGrpcServer.class).awaitTermination();
+            new CountDownLatch(1).await();
         } catch (InterruptedException exception) {
             Thread.currentThread().interrupt();
         }
